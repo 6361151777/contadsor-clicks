84efdc5b8c9fcb61a13fa3e9451dbecf50eb7f56
Tecnologías <pc-15@iMac-de-Tecnologias.local>
2024-03-09 12:50:36 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import './App.css';
+import Contador from'./componentes/Contador';
+import Boton from './componentes/Boton';
+import { useState } from 'react';
+
+function App() {
+  return (
+     <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App; 
